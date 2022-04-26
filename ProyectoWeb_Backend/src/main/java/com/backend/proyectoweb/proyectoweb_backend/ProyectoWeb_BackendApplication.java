607f39ccab96ecc40cc995deb9d78d8d2543c2d5
Santiago Barbosa <santibar15@hotmail.com>
2022-04-25 23:49:20 -0500

    Update

--- ProyectoWeb_Backend/src/main/java/com/backend/proyectoweb/proyectoweb_backend/ProyectoWeb_BackendApplication.java
+++ ProyectoWeb_Backend/src/main/java/com/backend/proyectoweb/proyectoweb_backend/ProyectoWeb_BackendApplication.java
@@ -1,4 +1,4 @@
-package com.backend.projectoweb.proyectoweb_backend;
+package com.backend.proyectoweb.proyectoweb_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
